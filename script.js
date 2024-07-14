bc8cbd1b026a0162dffc4035a93bfe0070a9f451
mainulbinalif <mynulislam4432@gmail.com>
2024-07-14 16:54:22 +0600

    update on localstorage

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ const inputFields = document.querySelectorAll('.goal-input')
 // const errorText = document.querySelector('.error-div')
 const errorText = document.querySelector('.error-label')
 const progressValue = document.querySelector('.progress-value')
-const goalInput = document.querySelectorAll('.goal-input')
 
-
+const allGoals = JSON.parse(localStorage.getItem('allGoals')) || {}
 
 checkboxList.forEach((checkbox) =>{
     checkbox.addEventListener('click', (e)=>{
@@ -13,19 +12,35 @@ checkboxList.forEach((checkbox) =>{
             return input.value
         })
         
-        if (!allGoalsAdded) {
-            errorText.classList.remove('invisible')
-            
-        }else{
+        if (allGoalsAdded) {
             checkbox.parentElement.classList.toggle('completed')
             progressValue.style.width = '33%'
+            const inputId = checkbox.nextElementSibling.id
+            allGoals[inputId].isCompleted = !allGoals[inputId].isCompleted
+            localStorage.setItem('allGoals', JSON.stringify(allGoals))
+        }else{
+            errorText.classList.remove('invisible')
         }
     })
 })
 
 
 inputFields.forEach((input) =>{
+    input.value = allGoals[input.id].name
+
+    if(allGoals[input.id].isCompleted){
+        input.parentElement.classlist.add('completed')
+    }
+
     input.addEventListener('focus', () =>{
         errorText.classList.add('invisible')
+    })
+    
+    input.addEventListener('input', () =>{
+        allGoals[input.id] = {
+            name: input.value,
+            isCompleted: false
+        }
+        localStorage.setItem('allGoals', JSON.stringify(allGoals))
     })
 })
