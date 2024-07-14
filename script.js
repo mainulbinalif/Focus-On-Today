103dfd0e429ac49ffef2f538a3deacd199d80cdb
mainulbinalif <mynulislam4432@gmail.com>
2024-07-14 19:05:48 +0600

    completed the quotes section

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const checkboxList = document.querySelectorAll('.custom-checkbox')
 const inputFields = document.querySelectorAll('.goal-input')
-// const errorText = document.querySelector('.error-div')
+const progressLabel = document.querySelector('.progress-label')
 const errorText = document.querySelector('.error-label')
 const progressValue = document.querySelector('.progress-value')
+
+
+const allQuotes = [
+    'Raise the bar by completing your goals!',
+    'Well begun is half done!',
+    'Just a step away, keep going!',
+    'Whoa! You just completed all the goals, time for chill :D',
+    ]
+
 
 const allGoals = JSON.parse(localStorage.getItem('allGoals')) || {}
 let completedGoalsCount = Object.values(allGoals).filter((goal) =>goal.completed).length
 progressValue.style.width = `${completedGoalsCount / 3 * 100}%`
-
+progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 Completed`
+progressLabel.innerText = allQuotes[completedGoalsCount]
 
 checkboxList.forEach((checkbox) =>{
     checkbox.addEventListener('click', (e)=>{
@@ -20,8 +30,10 @@ checkboxList.forEach((checkbox) =>{
             const inputId = checkbox.nextElementSibling.id
             allGoals[inputId].completed = !allGoals[inputId].completed
             completedGoalsCount = Object.values(allGoals).filter((goal) =>goal.completed).length
+
             progressValue.style.width = `${completedGoalsCount / 3 * 100}%`
             progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 Completed`
+            progressLabel.innerText = allQuotes[completedGoalsCount]
 
             localStorage.setItem('allGoals', JSON.stringify(allGoals))
         }else{
