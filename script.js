50b34759fc445422449a8fe4fa35147916fd43b2
mainulbinalif <mynulislam4432@gmail.com>
2024-07-16 04:10:56 +0600

    made some changes and updated the web

--- script.js
+++ script.js
@@ -13,10 +13,25 @@ const allQuotes = [
     ]
 
 
+// const allGoals = JSON.parse(localStorage.getItem('allGoals')) || {
+//     first: {
+//         name: '',
+//         completed: false
+//     },
+//     second: {
+//         name: '',
+//         completed: false
+//     },
+//     third: {
+//         name: '',
+//         completed: false
+//     }
+// }
 const allGoals = JSON.parse(localStorage.getItem('allGoals')) || {}
+
 let completedGoalsCount = Object.values(allGoals).filter((goal) =>goal.completed).length
-progressValue.style.width = `${completedGoalsCount / 3 * 100}%`
-progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 Completed`
+progressValue.style.width = `${completedGoalsCount / inputFields.length * 100}%`
+progressValue.firstElementChild.innerText = `${completedGoalsCount} / ${inputFields.length} Completed`
 progressLabel.innerText = allQuotes[completedGoalsCount]
 
 checkboxList.forEach((checkbox) =>{
@@ -31,8 +46,8 @@ checkboxList.forEach((checkbox) =>{
             allGoals[inputId].completed = !allGoals[inputId].completed
             completedGoalsCount = Object.values(allGoals).filter((goal) =>goal.completed).length
 
-            progressValue.style.width = `${completedGoalsCount / 3 * 100}%`
-            progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 Completed`
+            progressValue.style.width = `${completedGoalsCount / inputFields.length * 100}%`
+            progressValue.firstElementChild.innerText = `${completedGoalsCount}/${inputFields.length} Completed`
             progressLabel.innerText = allQuotes[completedGoalsCount]
 
             localStorage.setItem('allGoals', JSON.stringify(allGoals))
@@ -44,13 +59,14 @@ checkboxList.forEach((checkbox) =>{
 
 
 inputFields.forEach((input) =>{
-    input.value = allGoals[input.id].name
+    if (allGoals[input.id]) {
+        input.value = allGoals[input.id].name
 
-    if(allGoals[input.id].completed){
-        input.parentElement.classList.add('completed')
+
+        if(allGoals[input.id].completed){
+            input.parentElement.classList.add('completed')
+        }
     }
-
-
     
     input.addEventListener('focus', () =>{
         errorText.classList.add('invisible')
@@ -59,14 +75,18 @@ inputFields.forEach((input) =>{
     
     input.addEventListener('input', (e) =>{
 
-        if(allGoals[input.id].completed){
+        if(allGoals[input.id] && allGoals[input.id].completed){
             e.target.value = allGoals[input.id].name
             return
         }
-
-        allGoals[input.id] = {
-            name: input.value,
-            completed: false
+        
+        if(allGoals[input.id]){ 
+            allGoals[input.id].name = input.value
+        }else{
+            allGoals[input.id]= {
+                name: input.value,
+                completed: false
+            }
         }
         localStorage.setItem('allGoals', JSON.stringify(allGoals))
     })
